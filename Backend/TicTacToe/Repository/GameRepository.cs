fae174f3462a5f9f321bd5d9f626e6acccaf56c8
Najem Hamo <najem.hamo@gmail.com>
2024-05-21 14:47:44 +0200

    New seperated inv for TicTacToe Game

---
+++ Backend/TicTacToe/Repository/GameRepository.cs
@@ -0,0 +1,17 @@
+using Models;
+
+namespace Repository
+{
+    public class GameRepository : IGameRepository
+    {
+        public async Task<IEnumerable<GameRoom>> GetGameRooms()
+        {
+            return await Task.FromResult(GameRoom.GameRooms);
+        }
+
+        public async Task<GameRoom> GetGameRoom(Guid roomId)
+        {
+            return await Task.FromResult(GameRoom.GameRooms.FirstOrDefault(x => x.RoomId == roomId));
+        }
+    }
+}
---
+++ Backend/TicTacToe/Services/WebSocketService.cs
@@ -0,0 +1,8 @@
+using System.Net.WebSockets;
+using Models;
+
+namespace Services
+{
+
+
+}
