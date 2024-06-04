728fbd40a9d8f7cb59b987cc7331e4a49e91acc4
guro18 <rollgustav95@gmail.com>
2024-06-04 14:51:16 +0200

    added functionality for a more detailed scorepage

--- Frontend/Quiz/QuizHomePage.jsx
+++ Frontend/Quiz/QuizHomePage.jsx
@@ -6,13 +6,14 @@ export default function QuizHomePage() {
     const [descriptions, setDescriptions] = useState([]);
     const [answers, setAnswers] = useState ([]);
     const [userAnswers, setUserAnswers] = useState([]);
+    const [rightAnswers, setRightAnswers] = useState([]);
 
     const navigate = useNavigate();
 
     const startQuiz = () => {
         const initialUserAnswers = new Array(15).fill(null);
         setUserAnswers(initialUserAnswers);
-        navigate('/Quiz/TakeQuiz/1', {state: {questions, descriptions, answers, userAnswers : initialUserAnswers}});
+        navigate('/Quiz/TakeQuiz/1', {state: {questions, descriptions, answers, userAnswers : initialUserAnswers, rightAnswers}});
     };
 
     const backToHomePage = () => {
@@ -25,27 +26,33 @@ export default function QuizHomePage() {
 
     const fetchQuizData = async () => {
         try {
-            const [questionsResponse, descriptionsResponse, answersResponse] = await Promise.all([
+            const [questionsResponse, descriptionsResponse, answersResponse, rightAnswersResponse] = await Promise.all([
                 fetch('http://localhost:5007/Quiz/questions'),
                 fetch('http://localhost:5007/Quiz/descriptions'),
-                fetch('http://localhost:5007/Quiz/answers')
+                fetch('http://localhost:5007/Quiz/answers'),
+                fetch('http://localhost:5007/Quiz/rightAnswers')
             ]);
 
-            if (!questionsResponse.ok || !descriptionsResponse.ok || !answersResponse.ok)
+            if (!questionsResponse.ok || 
+                !descriptionsResponse.ok || 
+                !answersResponse.ok || 
+                !rightAnswersResponse.ok)
                 {
                 throw new Error('Failed to fetch quiz data');
                 }
 
-            const [questionData, descriptionData, answersData] = await Promise.all([
+            const [questionData, descriptionData, answersData, rightAnswersData] = await Promise.all([
                 questionsResponse.json(),
                 descriptionsResponse.json(),
-                answersResponse.json()
+                answersResponse.json(),
+                rightAnswersResponse.json()
             ]);
 
             setQuestions(questionData);
             setDescriptions(descriptionData);
             setAnswers(answersData);
-        } 
+            setRightAnswers(rightAnswersData);
+        }
         
         catch (error) 
         {
@@ -64,7 +71,7 @@ export default function QuizHomePage() {
                     Quiz
                 </h1>
             </header>
-            <main>
+            <main className="button-container">
                 <button onClick={startQuiz}>
                     Start Quiz
                 </button>
